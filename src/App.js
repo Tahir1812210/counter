ac800848da7ca5810bab73768998590ec5e07245
Tahirid1812210 <cs1812210@szabist.pk>
2023-10-24 14:35:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleAddCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrementCounter = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
+  const handleDecrementByFive = () => {
+    if (counter > 0) {
+      setCounter(counter - 5);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <span
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: 100,
+        }}
+      >
+        {counter}
+      </span>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <button
+          onClick={handleDecrementByFive}
+          style={{
+            backgroundColor: 'grey',
+            width: '200px',
+            height: '50px',
+            borderRadius: '10px',
+            margin: 100,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          -5
+        </button>
+        <button
+          onClick={handleDecrementCounter}
+          style={{
+            backgroundColor: 'grey',
+            width: '200px',
+            height: '50px',
+            borderRadius: '10px',
+            margin: 100,
+          }}
+        >
+          -1
+        </button>
+        <button
+          onClick={handleAddCounter}
+          style={{
+            backgroundColor: 'grey',
+            width: '200px',
+            height: '50px',
+            borderRadius: '10px',
+            margin: 100,
+          }}
+        >
+          +1
+        </button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
